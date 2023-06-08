7110b1ad622dd972545fadd986bde70b15ae2695
saad raja <shraja7@gmail.com>
2023-06-08 17:57:35 -0500

    add password hashing using bcrypt

--- api/index.js
+++ api/index.js
@@ -4,11 +4,15 @@ const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
 const User = require("./models/User");
+const bcrypt = require("bcrypt");
 require("dotenv").config({ path: "../.env" });
 
 //middleware including parsing json
 app.use(express.json());
 app.use(cors());
+
+//hash password
+const salt = 10;
 
 //connect to mongoose using url from .env file
 mongoose.connect(process.env.MONGO_URL);
@@ -18,7 +22,10 @@ app.post("/register", async (req, res) => {
   const { username, password } = req.body;
   //create new user, do try catch to catch errors
   try {
-    const userDoc = await User.create({ username, password });
+    const userDoc = await User.create({
+      username,
+      password: bcrypt.hashSync(password, salt),
+    });
     res.json(userDoc);
   } catch (error) {
     res.status(400).json({ message: error.message });
