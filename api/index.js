32996fa63c8cec83653d258c6beb35735a274a48
saad raja <shraja7@gmail.com>
2023-06-08 18:16:35 -0500

    implement tokens for logging in

--- api/index.js
+++ api/index.js
@@ -5,6 +5,8 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const User = require("./models/User");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
 require("dotenv").config({ path: "../.env" });
 
 //middleware including parsing json
@@ -33,12 +35,32 @@ app.post("/register", async (req, res) => {
 });
 
 //route for login
+// ... Your existing code ...
+
 app.post("/login", async (req, res) => {
-  //respond with user and password from request body
   const { username, password } = req.body;
-  //grab user
   const userDoc = await User.findOne({ username });
-  res.json(userDoc);
+
+  bcrypt.compare(password, userDoc.password, async (err, result) => {
+    if (err) {
+      res.status(400).json({ message: err.message });
+    } else if (result) {
+      try {
+        const token = jwt.sign(
+          { username, id: userDoc._id },
+          process.env.JWT_SECRET,
+          { expiresIn: "1h" } // Set token expiration time
+        );
+        res.json({ token });
+      } catch (error) {
+        res.status(400).json({ message: error.message });
+      }
+    } else {
+      res.status(400).json({ message: "Login failed" });
+    }
+  });
 });
+
+// ... Rest of your code ...
 
 app.listen(4000, () => console.log("listening at 4000"));
